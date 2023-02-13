0159e23dd1bf07efa0aaa45ecec208e6af7c6244
michaelKle <3406283+michaelKle@users.noreply.github.com>
2023-02-13 09:34:14 +0100

    First day

---
+++ Day_001/index.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+city = input("What is the name of the city you grew up in? \n")
+pet = input("What is the name of your first pet? \n")
+print("How about '" + city + " " + pet + "' ?" )
